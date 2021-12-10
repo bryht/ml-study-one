cf673929d77cb3481d5153a994d0297f495955ba
Ming Li <ming.li@dephion.com>
2021-12-10 10:06:31 +0100

    wip

--- src/data-input-api/main.py
+++ src/data-input-api/main.py
@@ -1,19 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from pymongo import MongoClient
+from decouple import config
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return { "title": "Welcome!" }
-
-
 class Item(BaseModel):
-    timestamp : str
+    timestamp: str
     humidity: float
     temperature: float
+
+
 @app.post("/")
 def create_item(item: Item):
-    
-    return item
+    client = MongoClient(config('MONGODB_URL'))
+    db = client.pico
+    db_item= {
+        'time': item.timestamp,
+        'humidity': item.humidity,
+        'temperature': item.temperature
+    }
+    result=db.picodata.insert_one(db_item)
+    return str(result.inserted_id)
--- src/data-output-api/main.py
+++ src/data-output-api/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
+from decouple import config
+from bson.json_util import dumps
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "title": "Welcome!" }
+    client = MongoClient(config('MONGODB_URL'))
+    db = client.pico
+    result=db.picodata.find({})
+    return dumps(result)
